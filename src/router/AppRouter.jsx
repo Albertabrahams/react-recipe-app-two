94120369142e45d59e2aeab930302b764d6fb059
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-04-08 22:45:21 +0300

    project 1.1

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,11 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "../components/navbar/Navbar";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import Details from "../pages/details/Details";
+import Login from '../pages/login/Login';
 
 
 
@@ -8,7 +13,13 @@ const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
-     
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/details" element={<Details/>}/>  
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
